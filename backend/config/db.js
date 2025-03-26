4681321f1a85a393449d5aa916597f3e690ffe76
shahjugal067 <shahjugal067@gmail.com>
2025-03-26 13:48:34 +0545

    changes

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+
+import mongoose from "mongoose";
+
+const connectDB = async () =>{
+    try {
+        await mongoose.connect(process.env.MONGO_URI).then(
+            () => {
+                console.log("Connected to DB")
+            }
+        )
+    } catch (error) {
+        console.log(error)
+    }
+};
+export default connectDB
---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import path from 'path'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser';
+import connectDB from './config/db.js'
+dotenv.config();
+
+const app = express()
+
+connectDB()
+const port = process.env.PORT || 5000
+app.use(express.json())
+app.use(cors())
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.listen(port,()=>{
+    console.log(`Example app listening on port ${port}`)
+})
